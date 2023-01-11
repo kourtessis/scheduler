b2e8b72f26c8bc7b81bf179b2900e13c7837f9be
Krista Kourtessis <krista.kourtessis@telus.com>
2023-01-11 14:41:42 -0800

    add appointment folder with stories for each

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import "components/Appointment/styles.scss"; 
+
+export default function Appointment( props ) {
+  // const { } = props;
+
+  return (
+    <article className="appointment"></article>
+  );
+}
